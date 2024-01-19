7cfb8afe9669e8b2065583cecfa7e8ddccbc985f
NancyElidah <nancyandriamahanintsoa@gmail.com>
2024-01-19 16:33:07 +0100

    Update boite

--- src/main/java/com/pack/fiaraoccaz/controller/BoiteVitesseController.java
+++ src/main/java/com/pack/fiaraoccaz/controller/BoiteVitesseController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import com.pack.fiaraoccaz.dao.BoiteVitesseDao;
 import com.pack.fiaraoccaz.model.BoiteVitesse;
 
-@CrossOrigin("http://localhost:3000")
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/bv")
 public class BoiteVitesseController {
