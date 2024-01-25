2c33d67322f3f49de622062dde81bee055ade055
nirs0204 <157597847+NirisoaLala@users.noreply.github.com>
2024-01-25 10:09:18 +0300

    filtre annonce frontOffice

--- src/main/java/com/pack/fiaraoccaz/controller/V_AnnonceController.java
+++ src/main/java/com/pack/fiaraoccaz/controller/V_AnnonceController.java
@@ -20,4 +20,49 @@ public class V_AnnonceController {
     public List<V_Annonce> findAllByEtat(@PathVariable int etat){
         return vaDao.findAllByEtat(etat);
     }
+
+    @GetMapping("/type/{idtype}")
+    public List<V_Annonce> findAllByIdtype(@PathVariable int idtype){
+        return vaDao.findAllByIdtype(idtype);
+    }
+
+    @GetMapping("/marque/{idmarque}")
+    public List<V_Annonce> findAllByIdmarque(@PathVariable int idmarque){
+        return vaDao.findAllByIdmarque(idmarque);
+    }
+
+    @GetMapping("/modele/{idmodele}")
+    public List<V_Annonce> findAllByIdmodele(@PathVariable int idmodele){
+        return vaDao.findAllByIdmodele(idmodele);
+    }
+
+    @GetMapping("/energie/{idenergie}")
+    public List<V_Annonce> findAllByIdenergie(@PathVariable int idenergie){
+        return vaDao.findAllByIdenergie(idenergie);
+    }
+
+    @GetMapping("/couleur/{idcouleur}")
+    public List<V_Annonce> findAllByIdcouleur(@PathVariable int idcouleur){
+        return vaDao.findAllByIdcouleur(idcouleur);
+    }
+
+    @GetMapping("/pays/{idpays}")
+    public List<V_Annonce> findAllByIdpays(@PathVariable int idpays){
+        return vaDao.findAllByIdpays(idpays);
+    }
+
+    @GetMapping("/boite_vitesse/{idbv}")
+    public List<V_Annonce> findAllByIdbv(@PathVariable int idbv){
+        return vaDao.findAllByIdbv(idbv);
+    }
+
+    @GetMapping("/prix/{prixmin}/{prixmax}")
+    public List<V_Annonce> findAllByPrixBetween(@PathVariable("prixmin") double prixMin, @PathVariable("prixmax") double prixMax){
+        return vaDao.findAllByPrixBetween(prixMin, prixMax);
+    }
+
+    @GetMapping("/annee/{anneemin}/{anneemax}")
+    public List<V_Annonce> findAllByAnneeBetween(@PathVariable("anneemin") double anneeMin, @PathVariable("anneemax") double anneeMax){
+        return vaDao.findAllByAnneeBetween(anneeMin, anneeMax);
+    }
 }
--- src/main/java/com/pack/fiaraoccaz/model/V_Annonce.java
+++ src/main/java/com/pack/fiaraoccaz/model/V_Annonce.java
@@ -14,14 +14,24 @@ public class V_Annonce {
     int iduser;
     @Column(name = "idvoiture")
     int idvoiture;
+    @Column(name = "idtype")
+    int idtype;
     @Column(name = "type")
     String type;
+    @Column(name = "idmarque")
+    int idmarque;
     @Column(name = "marque")
     String marque;
+    @Column(name = "idmodele")
+    int idmodele;
     @Column(name = "modele")
     String modele;
+    @Column(name = "idenergie")
+    int idenergie;
     @Column(name = "energie")
     String energie;
+    @Column(name = "idbv")
+    int idbv;
     @Column(name = "boite_vitesse")
     String boite_vitesse;
     @Column(name = "annee")
@@ -30,8 +40,12 @@ public class V_Annonce {
     double kilometrage;
     @Column(name = "prix")
     double prix;
+    @Column(name = "idcouleur")
+    int idcouleur;
     @Column(name = "couleur")
     String couleur;
+    @Column(name = "idpays")
+    int idpays;
     @Column(name = "provenance")
     String provenance;
     @Column(name = "nbplace")
@@ -59,11 +73,23 @@ public class V_Annonce {
     public void setIdvoiture(int idvoiture) {
         this.idvoiture = idvoiture;
     }
+    public int getIdtype() {
+        return idtype;
+    }
+    public void setIdtype(int idtype) {
+        this.idtype = idtype;
+    }
     public String getType() {
         return type;
     }
     public void setType(String type) {
         this.type = type;
+    }
+    public int getIdmarque() {
+        return idmarque;
+    }
+    public void setIdmarque(int idmarque) {
+        this.idmarque = idmarque;
     }
     public String getMarque() {
         return marque;
@@ -71,17 +97,35 @@ public class V_Annonce {
     public void setMarque(String marque) {
         this.marque = marque;
     }
+    public int getIdmodele() {
+        return idmodele;
+    }
+    public void setIdmodele(int idmodele) {
+        this.idmodele = idmodele;
+    }
     public String getModele() {
         return modele;
     }
     public void setModele(String modele) {
         this.modele = modele;
     }
+    public int getIdenergie() {
+        return idenergie;
+    }
+    public void setIdenergie(int idenergie) {
+        this.idenergie = idenergie;
+    }
     public String getEnergie() {
         return energie;
     }
     public void setEnergie(String energie) {
         this.energie = energie;
+    }
+    public int getIdbv() {
+        return idbv;
+    }
+    public void setIdbv(int idbv) {
+        this.idbv = idbv;
     }
     public String getBoite_vitesse() {
         return boite_vitesse;
@@ -107,11 +151,23 @@ public class V_Annonce {
     public void setPrix(double prix) {
         this.prix = prix;
     }
+    public int getIdcouleur() {
+        return idcouleur;
+    }
+    public void setIdcouleur(int idcouleur) {
+        this.idcouleur = idcouleur;
+    }
     public String getCouleur() {
         return couleur;
     }
     public void setCouleur(String couleur) {
         this.couleur = couleur;
+    }
+    public int getIdpays() {
+        return idpays;
+    }
+    public void setIdpays(int idpays) {
+        this.idpays = idpays;
     }
     public String getProvenance() {
         return provenance;
