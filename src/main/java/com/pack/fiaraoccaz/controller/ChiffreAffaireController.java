853dbde705f2ae2deadbf47d21d17c15cba51720
Nyaina03 <120169509+Nyaina03@users.noreply.github.com>
2024-01-26 17:01:48 +0300

    Update ChiffreAffaireController.java

--- src/main/java/com/pack/fiaraoccaz/controller/ChiffreAffaireController.java
+++ src/main/java/com/pack/fiaraoccaz/controller/ChiffreAffaireController.java
@@ -5,6 +5,10 @@ import com.pack.fiaraoccaz.entity.VenteMensuelle;
 import com.pack.fiaraoccaz.entity.ChiffreAffaire;
 import com.pack.fiaraoccaz.dto.ChiffreAffaireRequest;
 import com.pack.fiaraoccaz.service.ChiffreAffaireService;
+import com.pack.fiaraoccaz.entity.Token;
+import com.pack.fiaraoccaz.entity.User;
+import com.pack.fiaraoccaz.repository.TokenRepository;
+import com.pack.fiaraoccaz.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,45 +21,95 @@ public class ChiffreAffaireController {
 
     @Autowired
     private ChiffreAffaireService chiffreAffaireService;
+    @Autowired
+    private TokenRepository tokenRe;
+    @Autowired
+    private UserService userService;
 
-      // Endpoint pour enregistrer un nouveau chiffre d'affaire
-      @PostMapping("/nouveau-chiffre-affaire")
-      public ChiffreAffaire enregistrerChiffreAffaire(@RequestBody ChiffreAffaireRequest request) {
-          return chiffreAffaireService.enregistrerChiffreAffaire(
-                  request.getPrixVente(),
-                  request.getEtatAnnonce(),
-                  request.getCommission()
-          );
-      }
-  
-      // Endpoint pour récupérer tous les chiffres d'affaires enregistrés
-      @GetMapping("/chiffres-affaires")
-      public List<ChiffreAffaire> getAllChiffresAffaires() {
-          return chiffreAffaireService.getAllChiffresAffaires();
-      }
-  
-      // Endpoint pour calculer le chiffre d'affaire net
-      @GetMapping("/calcul-chiffre-affaire-net")
-      public double calculerChiffreAffaireNet(@RequestParam double prixVente, @RequestParam int etatAnnonce, @RequestParam double commission) {
-          return chiffreAffaireService.calculerChiffreAffaireNet(prixVente, etatAnnonce, commission);
-      }
+    // Endpoint pour enregistrer un nouveau chiffre d'affaire
+    @PostMapping("/nouveau-chiffre-affaire")
+    public ChiffreAffaire enregistrerChiffreAffaire(@RequestBody ChiffreAffaireRequest request,
+                                                    @RequestParam("token") String token,
+                                                    @RequestParam("id") String idU) throws Exception {
+        Token tok = tokenRe.findIdUtilsateurFromToken(token);
+        Long id = Long.valueOf(idU);
+
+        User user = userService.findUser(id);
+        if (tok != null && tok.isValid(id) && user.getEtat() == 10) {
+            return chiffreAffaireService.enregistrerChiffreAffaire(
+                    request.getPrixVente(),
+                    request.getEtatAnnonce(),
+                    request.getCommission()
+            );
+        }
+        return null; 
+    }
+
+    // Endpoint pour récupérer tous les chiffres d'affaires enregistrés
+    @GetMapping("/chiffres-affaires")
+    public List<ChiffreAffaire> getAllChiffresAffaires(@RequestParam("token") String token,
+                                                       @RequestParam("id") String idU) throws Exception {
+        Token tok = tokenRe.findIdUtilsateurFromToken(token);
+        Long id = Long.valueOf(idU);
+
+        User user = userService.findUser(id);
+        if (tok != null && tok.isValid(id) && user.getEtat() == 10) {
+            return chiffreAffaireService.getAllChiffresAffaires();
+        }
+        return null; 
+    }
+
+    // Endpoint pour calculer le chiffre d'affaire net
+    @GetMapping("/calcul-chiffre-affaire-net")
+    public double calculerChiffreAffaireNet(@RequestParam double prixVente,
+                                            @RequestParam int etatAnnonce,
+                                            @RequestParam double commission,
+                                            @RequestParam("token") String token,
+                                            @RequestParam("id") String idU) throws Exception {
+        Token tok = tokenRe.findIdUtilsateurFromToken(token);
+        Long id = Long.valueOf(idU);
+
+        User user = userService.findUser(id);
+        if (tok != null && tok.isValid(id) && user.getEtat() == 10) {
+            return chiffreAffaireService.calculerChiffreAffaireNet(prixVente, etatAnnonce, commission);
+        }
+        return 0;
+    }
 
     // Endpoint pour enregistrer une nouvelle vente mensuelle
     @PostMapping("/vente-mensuelle")
-    public VenteMensuelle enregistrerVenteMensuelle(@RequestBody VenteMensuelle venteMensuelle) {
-        return chiffreAffaireService.enregistrerVenteMensuelle(
-                venteMensuelle.getChiffreAffaire(),
-                venteMensuelle.getMois(),
-                venteMensuelle.getAnnee(),
-                venteMensuelle.getType()
-        );
+    public VenteMensuelle enregistrerVenteMensuelle(@RequestBody VenteMensuelle venteMensuelle,
+                                                    @RequestParam("token") String token,
+                                                    @RequestParam("id") String idU) throws Exception {
+        Token tok = tokenRe.findIdUtilsateurFromToken(token);
+        Long id = Long.valueOf(idU);
+
+        User user = userService.findUser(id);
+        if (tok != null && tok.isValid(id) && user.getEtat() == 10) {
+            return chiffreAffaireService.enregistrerVenteMensuelle(
+                    venteMensuelle.getChiffreAffaire(),
+                    venteMensuelle.getMois(),
+                    venteMensuelle.getAnnee(),
+                    venteMensuelle.getType()
+            );
+        }
+        return null; 
     }
 
     // Endpoint pour récupérer les chiffres d'affaires mensuels par type
     @GetMapping("/ventes-mensuelles/{typeId}")
-    public List<VenteMensuelle> getChiffresAffairesMensuelsParType(@PathVariable Long typeId) {
-        Type type = new Type();
-        type.setIdType(typeId);
-        return chiffreAffaireService.getChiffresAffairesMensuelsParType(type);
+    public List<VenteMensuelle> getChiffresAffairesMensuelsParType(@PathVariable Long typeId,
+                                                                   @RequestParam("token") String token,
+                                                                   @RequestParam("id") String idU) throws Exception {
+        Token tok = tokenRe.findIdUtilsateurFromToken(token);
+        Long id = Long.valueOf(idU);
+
+        User user = userService.findUser(id);
+        if (tok != null && tok.isValid(id) && user.getEtat() == 10) {
+            Type type = new Type();
+            type.setIdType(typeId);
+            return chiffreAffaireService.getChiffresAffairesMensuelsParType(type);
+        }
+        return null; 
     }
 }
