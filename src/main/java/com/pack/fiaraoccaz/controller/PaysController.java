74baf5ad9164f9c76fd610bf2e9b78e28a4279c1
Nyaina03 <120169509+Nyaina03@users.noreply.github.com>
2024-01-26 17:12:11 +0300

    Update PaysController.java

--- src/main/java/com/pack/fiaraoccaz/controller/PaysController.java
+++ src/main/java/com/pack/fiaraoccaz/controller/PaysController.java
@@ -2,6 +2,10 @@ package com.pack.fiaraoccaz.controller;
 
 import com.pack.fiaraoccaz.entity.Pays;
 import com.pack.fiaraoccaz.service.PaysService;
+import com.pack.fiaraoccaz.entity.Token;
+import com.pack.fiaraoccaz.entity.User;
+import com.pack.fiaraoccaz.repository.TokenRepository;
+import com.pack.fiaraoccaz.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,29 +19,77 @@ public class PaysController {
 
     @Autowired
     private PaysService paysService;
+    @Autowired
+    private TokenRepository tokenRe;
+    @Autowired
+    private UserService userService;
 
-    @GetMapping
-    public List<Pays> getAllPays() {
-        return paysService.getAllPays();
+    @GetMapping("/{token}/getAll")
+    public List<Pays> getAllPays(@PathVariable("token") String token, @RequestParam("id") String idU) throws Exception {
+        Token tok = tokenRe.findIdUtilsateurFromToken(token);
+        Long id = Long.valueOf(idU);
+
+        User user = userService.findUser(id);
+        if (tok != null && tok.isValid(id) && user.getEtat() == 10) {
+            return paysService.getAllPays();
+        }
+        return null;  
     }
 
-    @GetMapping("/{id}")
-    public Optional<Pays> getPaysById(@PathVariable Long id) {
-        return paysService.getPaysById(id);
+    @GetMapping("/{token}/getById/{id}")
+    public Optional<Pays> getPaysById(@PathVariable Long id,
+                                      @PathVariable("token") String token,
+                                      @RequestParam("id") String idU) throws Exception {
+        Token tok = tokenRe.findIdUtilsateurFromToken(token);
+        id = Long.valueOf(idU);
+
+        User user = userService.findUser(id);
+        if (tok != null && tok.isValid(id) && user.getEtat() == 10) {
+            return paysService.getPaysById(id);
+        }
+        return Optional.empty(); 
     }
 
-    @PostMapping
-    public Pays createPays(@RequestBody Pays pays) {
-        return paysService.createPays(pays);
+    @PostMapping("/{token}/create")
+    public Pays createPays(@RequestBody Pays pays,
+                           @PathVariable("token") String token,
+                           @RequestParam("id") String idU) throws Exception {
+        Token tok = tokenRe.findIdUtilsateurFromToken(token);
+        Long id = Long.valueOf(idU);
+
+        User user = userService.findUser(id);
+        if (tok != null && tok.isValid(id) && user.getEtat() == 10) {
+            return paysService.createPays(pays);
+        }
+        return null;  
     }
 
-    @PutMapping("/{id}")
-    public Pays updatePays(@PathVariable Long id, @RequestBody Pays pays) {
-        return paysService.updatePays(id, pays);
+    @PutMapping("/{token}/update/{id}")
+    public Pays updatePays(@PathVariable Long id,
+                           @RequestBody Pays pays,
+                           @PathVariable("token") String token,
+                           @RequestParam("id") String idU) throws Exception {
+        Token tok = tokenRe.findIdUtilsateurFromToken(token);
+        id = Long.valueOf(idU);
+
+        User user = userService.findUser(id);
+        if (tok != null && tok.isValid(id) && user.getEtat() == 10) {
+            return paysService.updatePays(id, pays);
+        }
+        return null; 
     }
 
-    @DeleteMapping("/{id}")
-    public void deletePays(@PathVariable Long id) {
-        paysService.deletePays(id);
+    @DeleteMapping("/{token}/delete/{id}")
+    public void deletePays(@PathVariable Long id,
+                           @PathVariable("token") String token,
+                           @RequestParam("id") String idU) throws Exception {
+        Token tok = tokenRe.findIdUtilsateurFromToken(token);
+        id = Long.valueOf(idU);
+
+        User user = userService.findUser(id);
+        if (tok != null && tok.isValid(id) && user.getEtat() == 10) {
+            paysService.deletePays(id);
+        }
+       
     }
 }
